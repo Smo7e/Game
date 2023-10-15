56261d1b19e60df820827342c85ec2a930af9fc1
Smo7e <romayacovlev@gmail.com>
2023-10-15 19:31:36 +0400

    Update Game.tsx

--- client/src/component/Game/Game.tsx
+++ client/src/component/Game/Game.tsx
@@ -1,19 +1,23 @@
 import React from "react";
 import { Canvas } from "@react-three/fiber";
 const Game: React.FC = () => {
-  return (
-    <Canvas
-      className="canvas"
-      camera={{
-        position: [0, 0, 14],
-        zoom: 10,
-        near: 0.1,
-        far: 1000,
-      }}
-    >
-      <ambientLight intensity={0.1} />
-      <directionalLight color="red" position={[0, 0, 5]} />
-    </Canvas>
-  );
+    return (
+        <Canvas
+            className="canvas"
+            camera={{
+                position: [0, 0, 14],
+                zoom: 10,
+                near: 0.1,
+                far: 1000,
+            }}
+        >
+            <ambientLight intensity={0.1} />
+            <directionalLight color="red" position={[0, 0, 5]} />
+            <mesh>
+                <boxGeometry />
+                <meshStandardMaterial />
+            </mesh>
+        </Canvas>
+    );
 };
 export default Game;
