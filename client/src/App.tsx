a8b443f3df00dcaa9f818e5c6ca6b2f3a62da22b
Smo7e <romayacovlev@gmail.com>
2023-10-17 00:41:30 +0400

    add useState

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { HOST } from "./config";
 
@@ -6,12 +6,23 @@ import { Server } from "./modules";
 
 import "./App.css";
 import SignUp from "./component/SignUp/SignUp";
+import Game from "./component/Game/Game";
 export const ServerContext = React.createContext<Server>(null!);
+export enum EPAGES {
+    SIGNUP,
+    LOGIN,
+    GAME,
+}
 
 const App: React.FC = () => {
     const server = new Server(HOST);
+    const [epages, setEpages] = useState<EPAGES>(EPAGES.SIGNUP);
 
-    return <ServerContext.Provider value={server}></ServerContext.Provider>;
+    return (
+        <ServerContext.Provider value={server}>
+            {epages === EPAGES.SIGNUP ? <SignUp epages={setEpages} /> : epages === EPAGES.GAME ? <Game /> : <></>}
+        </ServerContext.Provider>
+    );
 };
 
 export default App;
