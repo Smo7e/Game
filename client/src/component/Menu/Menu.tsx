4d018bd340e7c24861324b6c36150d72b32229fc
Alina181 <132784042+Alina181@users.noreply.github.com>
2023-10-25 22:13:11 +0400

    Add files via upload

---
+++ client/src/component/Menu/Menu.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+import "./Menu.css";
+
+
+const Menu: React.FC = () => {
+  return (
+        <div className="mainMenu">
+          <button className="photo-button"></button>
+
+          <div className="profile-panel">
+            <button className="new-profile-button"></button>
+            <button className="text-button">Друзья</button>
+        </div>
+    
+          <div className="buttons-container">
+            <button className="button1">
+                    Играть
+            </button>
+
+            <button className="button2">Герои</button>
+            <button className="button3">Параметры</button>
+          </div>
+          <div>
+          <button className="button-account">Сменить аккаунт</button>
+          </div>
+        </div>
+       
+    
+  );
+};
+
+export default Menu;
