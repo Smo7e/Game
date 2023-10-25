fce4dfb9014ad810fc36c1a4ee785008e7780a61
Константин <kostyagreduagin.ru@gmail.com>
2023-10-26 00:58:14 +0400

    Adding a test Database connection and the getPersons function

--- server/application/modules/DB.php
+++ server/application/modules/DB.php
@@ -1,6 +1,32 @@
 <?php
-class DB
-{
-    
+class DB {
+    private $db;
+
+    function __construct() {
+        $username = 'mysql';
+        $password = 'mysql';
+        $db = 'pdo_example';
+        $host = 'server';
+
+        $dsn = 'mysql:host='.$host.';dbname'.$db.';charset=utf8;';
+
+        $this->db = new PDO($dsn, $username, $password);
+    }
+
+
+    function getPersons($token) {
+        if ($token) {
+            $query = 'SELECT * FROM persons';
+            $stmt = $this->db->prepare($query);
+            if ($stmt) {
+                $persons = $stmt->fetchAll(PDO::FETCH_ASSOC);
+                return $persons;
+            } else {
+                return array(false, 401);
+            }
+        } else {
+            return array(false, 1002);
+        }
+    }
 
 }
