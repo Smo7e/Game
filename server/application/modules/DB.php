6abdacb0177d9f543b47ebe4f526496309587146
AnnaTonkova <128217570+AnnaTonkova@users.noreply.github.com>
2023-10-26 13:32:43 +0400

    Update DB.php

--- server/application/modules/DB.php
+++ server/application/modules/DB.php
@@ -1,6 +1,27 @@
 <?php
 class DB
 {
-    
+    private $db = null;
+
+    function getUserById($id) {
+        $query='SELECT * FROM users WHERE id=?';
+        $stmt = $this->db->prepare($query);
+        $stmt->execute([$id]);
+        return $stmt->fetchObject();
+    }
+
+    function getUserByLogin($login) {
+        $query='SELECT * FROM users WHERE login=?';
+        $stmt = $this->db->prepare($query);
+        $stmt->execute([$login]);
+        return $stmt->fetchObject();
+    }
+
+    function getUserByToken($token) {
+        $query='SELECT id FROM users WHERE token=?';
+        $stmt = $this->db->prepare($query);
+        $stmt->execute([$token]);
+        return $stmt->fetchObject();
+    }
 
 }
