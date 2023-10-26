3012a6dc647744ca02844f5c5556fcaeb44b00dc
Ali Razan <razanali627@gmail.com>
2023-10-27 01:24:17 +0400

    users_table.

---
+++ server/resources/db/migration/users_table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    login VARCHAR(50) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    name VARCHAR(50) NOT NULL,
+    surname VARCHAR(50) NOT NULL,
+    token VARCHAR(255) NOT NULL
+);
