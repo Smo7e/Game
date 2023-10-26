b21df7c977cd6d15434ac418fe92798140b8388b
Smo7e <romayacovlev@gmail.com>
2023-10-26 23:03:36 +0400

    add animation walk

---
+++ client/src/modules/Game/Player.tsx
@@ -0,0 +1,73 @@
+import { useFrame, useLoader } from "@react-three/fiber";
+import React, { useEffect, useState } from "react";
+import { useRef } from "react";
+import { Mesh, TextureLoader } from "three";
+import { sportikDown, sportikRight, sportikLeft, sportikUp } from "./index";
+
+const Player: React.FC = () => {
+    const personRef = useRef<Mesh>(null);
+    const [directionPlayer, setdirectionPlayer] = useState(sportikDown[0]);
+    const [currentFrame, setCurrentFrame] = useState(0);
+    const [controls, setControls] = useState({
+        w: false,
+        s: false,
+        a: false,
+        d: false,
+    });
+    useFrame((state) => {
+        const { w, a, s, d } = controls;
+        console.log(state);
+        if (w) {
+            setCurrentFrame((frame) => (frame + 0.1) % sportikUp.length);
+            setdirectionPlayer(sportikUp[Math.floor(currentFrame)]);
+            personRef.current!.position.y += 0.03;
+        }
+        if (s) {
+            setCurrentFrame((frame) => (frame + 0.1) % sportikDown.length);
+            setdirectionPlayer(sportikDown[Math.floor(currentFrame)]);
+            personRef.current!.position.y -= 0.03;
+        }
+        if (a) {
+            setCurrentFrame((frame) => (frame + 0.1) % sportikLeft.length);
+            setdirectionPlayer(sportikLeft[Math.floor(currentFrame)]);
+            personRef.current!.position.x -= 0.03;
+        }
+        if (d) {
+            setCurrentFrame((frame) => (frame + 0.1) % sportikRight.length);
+            setdirectionPlayer(sportikRight[Math.floor(currentFrame)]);
+            personRef.current!.position.x += 0.03;
+        }
+    });
+    const keydownHangler = (e: any) => {
+        const key = e.key.toLowerCase();
+        setControls((controls) => ({
+            ...controls,
+            [key]: true,
+        }));
+    };
+    const keyUpHangler = (e: any) => {
+        const key = e.key.toLowerCase();
+        setControls((controls) => ({
+            ...controls,
+            [key]: false,
+        }));
+    };
+
+    useEffect(() => {
+        window.addEventListener("keydown", keydownHangler);
+        window.addEventListener("keyup", keyUpHangler);
+        return () => {
+            window.removeEventListener("keydown", keydownHangler);
+            window.removeEventListener("keyup", keyUpHangler);
+        };
+    });
+    return (
+        <>
+            <mesh ref={personRef}>
+                <planeGeometry />
+                <meshStandardMaterial map={useLoader(TextureLoader, directionPlayer)} transparent />
+            </mesh>
+        </>
+    );
+};
+export default Player;
---
+++ client/src/modules/Game/index.ts
@@ -0,0 +1,72 @@
+import sportikDown0 from "./image/Sportik/sportikDown1.png";
+import sportikDown1 from "./image/Sportik/sportikDown1.png";
+import sportikDown2 from "./image/Sportik/sportikDown2.png";
+import sportikDown3 from "./image/Sportik/sportikDown3.png";
+import sportikDown4 from "./image/Sportik/sportikDown4.png";
+import sportikDown5 from "./image/Sportik/sportikDown5.png";
+import sportikDown6 from "./image/Sportik/sportikDown6.png";
+import sportikDown7 from "./image/Sportik/sportikDown7.png";
+import sportikDown8 from "./image/Sportik/sportikDown8.png";
+
+import sportikUp0 from "./image/Sportik/sportikUp0.png";
+import sportikUp1 from "./image/Sportik/sportikUp1.png";
+import sportikUp2 from "./image/Sportik/sportikUp2.png";
+import sportikUp3 from "./image/Sportik/sportikUp3.png";
+import sportikUp4 from "./image/Sportik/sportikUp4.png";
+import sportikUp5 from "./image/Sportik/sportikUp5.png";
+import sportikUp6 from "./image/Sportik/sportikUp6.png";
+import sportikUp7 from "./image/Sportik/sportikUp7.png";
+import sportikUp8 from "./image/Sportik/sportikUp8.png";
+
+import sportikLeft0 from "./image/Sportik/sportikLeft0.png";
+import sportikLeft1 from "./image/Sportik/sportikLeft1.png";
+import sportikLeft2 from "./image/Sportik/sportikLeft2.png";
+import sportikLeft3 from "./image/Sportik/sportikLeft3.png";
+import sportikLeft4 from "./image/Sportik/sportikLeft4.png";
+import sportikLeft5 from "./image/Sportik/sportikLeft5.png";
+import sportikLeft6 from "./image/Sportik/sportikLeft6.png";
+import sportikLeft7 from "./image/Sportik/sportikLeft7.png";
+import sportikLeft8 from "./image/Sportik/sportikLeft8.png";
+
+import sportikRight0 from "./image/Sportik/sportikRight0.png";
+import sportikRight1 from "./image/Sportik/sportikRight1.png";
+import sportikRight2 from "./image/Sportik/sportikRight2.png";
+import sportikRight3 from "./image/Sportik/sportikRight3.png";
+import sportikRight4 from "./image/Sportik/sportikRight4.png";
+import sportikRight5 from "./image/Sportik/sportikRight5.png";
+import sportikRight6 from "./image/Sportik/sportikRight6.png";
+import sportikRight7 from "./image/Sportik/sportikRight7.png";
+import sportikRight8 from "./image/Sportik/sportikRight8.png";
+
+const sportikDown = [
+    sportikDown1,
+    sportikDown2,
+    sportikDown3,
+    sportikDown4,
+    sportikDown5,
+    sportikDown6,
+    sportikDown7,
+    sportikDown8,
+];
+const sportikRight = [
+    sportikRight1,
+    sportikRight2,
+    sportikRight3,
+    sportikRight4,
+    sportikRight5,
+    sportikRight6,
+    sportikRight7,
+    sportikRight8,
+];
+const sportikUp = [sportikUp1, sportikUp2, sportikUp3, sportikUp4, sportikUp5, sportikUp6, sportikUp7, sportikUp8];
+const sportikLeft = [
+    sportikLeft1,
+    sportikLeft2,
+    sportikLeft3,
+    sportikLeft4,
+    sportikLeft5,
+    sportikLeft6,
+    sportikLeft7,
+    sportikLeft8,
+];
+export { sportikDown, sportikRight, sportikUp, sportikLeft };
