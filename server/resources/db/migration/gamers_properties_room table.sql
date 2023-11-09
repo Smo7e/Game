8a709928047641298235fb9f7d9bb8e20c144ea0
Ali Razan <razanali627@gmail.com>
2023-11-09 18:16:18 +0400

    исправлено

--- server/resources/db/migration/gamers_properties_room table.sql
+++ server/resources/db/migration/gamers_properties_room table.sql
@@ -1,12 +1,11 @@
 CREATE TABLE gamers_properties (
     id INT AUTO_INCREMENT PRIMARY KEY,
     gamer_id INT,
-    health FLOAT,
-    mass FLOAT,
-    speed FLOAT
-    FOREIGN KEY (gamer_id) REFERENCES gamers (id)
+    `key` VARCHAR(255),
+    name VARCHAR(255),
+    value VARCHAR(255),
+    FOREIGN KEY (gamer_id) REFERENCES gamers(id)
 );
-
 
 CREATE TABLE room (
     id INT AUTO_INCREMENT PRIMARY KEY,
