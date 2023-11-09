c7e2399c27614dc3d1ab6195d6a69f366d44480e
Semen <IvanovSA2309@gmail.com>
2023-11-09 16:56:33 +0400

    eshe odin fix

--- client/src/component/Login/Login.tsx
+++ client/src/component/Login/Login.tsx
@@ -12,7 +12,7 @@ return (
             <form>
                 <input className='loginInput' placeholder='Логин'/><br />
                 <input className='loginInput' placeholder='Пароль'/><br />
-                <input type='checkbox' className='checkbox'/><label className='checkboxText'>Не выходить из учетной записи</label><br />
+                <input type='checkbox' id='galka' className='checkbox'/><label className='checkboxText' htmlFor='galka'>Не выходить из учетной записи</label><br />
                 <button className='loginButton'>Продолжить</button><br />
                 <hr />
                 <div className='otherButtons'>
