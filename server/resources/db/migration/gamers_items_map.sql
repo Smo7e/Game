ac49c1142cb9dddcd3823f04d6b449e0953de934
Ali Razan <razanali627@gmail.com>
2023-11-09 18:02:04 +0400

    исправлено

--- server/resources/db/migration/gamers_items_map.sql
+++ server/resources/db/migration/gamers_items_map.sql
@@ -2,14 +2,17 @@ CREATE TABLE gamers_items (
     id INT AUTO_INCREMENT PRIMARY KEY,
     item_id INT,
     gamer_id INT,
-    status VARCHAR(50),
+    status ENUM('надето', 'в кармане', NULL),
     x FLOAT,
     y FLOAT,
     FOREIGN KEY (item_id) REFERENCES items(id),
     FOREIGN KEY (gamer_id) REFERENCES gamers(id)
 );
+
 CREATE TABLE map (
+    room_id INT, 
     height INT,
     width INT,
-    content TEXT
+    content TEXT,
+    FOREIGN KEY (room_id) REFERENCES room(id)
 );
