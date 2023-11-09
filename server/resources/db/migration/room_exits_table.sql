389f57c072ecc696f46ef84e071ebd45945f2562
Ali Razan <razanali627@gmail.com>
2023-11-09 21:26:29 +0400

    room_exits table.

---
+++ server/resources/db/migration/room_exits_table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE room_exits (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    room_id INT,
+    position VARCHAR(255),
+    goto_id INT,
+    status ENUM('открыто', 'закрыто', NULL),
+    FOREIGN KEY (room_id) REFERENCES room(id),
+    FOREIGN KEY (goto_id) REFERENCES room(id)
+);
