4ac1211c2776ccddb0691a0b54911d4d07eb93d4
Ali Razan <razanali627@gmail.com>
2023-11-12 22:23:01 +0400

    метод setPerson

--- server/application/modules/Lobby.php
+++ server/application/modules/Lobby.php
@@ -13,4 +13,14 @@ class Lobby {
         }
         return array(false, 1002);
     }
+    
+    public function setPerson($params) {
+        $id = $params['id'];
+        $idPerson = $params['person_id'];
+        $token = $params['token'];
+        if ($id && $idPerson && $token) {
+            return $this->db->setPerson($id, $idPerson, $token);
+        }
+        return array(false, 1002);
+    }
 }
