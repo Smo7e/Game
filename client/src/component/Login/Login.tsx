109a096db65582bfdb7dc2ed7fe4ae1d3c6368b6
Semen <IvanovSA2309@gmail.com>
2023-11-13 17:36:49 +0400

    hr fix

--- client/src/component/Login/Login.tsx
+++ client/src/component/Login/Login.tsx
@@ -14,7 +14,7 @@ return (
                 <input className='loginInput' placeholder='Пароль'/><br />
                 <input type='checkbox' id='galka' className='checkbox'/><label className='checkboxText' htmlFor='galka'>Не выходить из учетной записи</label><br />
                 <button className='loginButton'>Продолжить</button><br />
-                <hr />
+                <hr className='hrLogin'/>
                 <div className='otherButtonsLogin'>
                     <button className='otherButtonLogin'>Не можете войти?</button><br />
                     <button className='otherButtonLogin'>Создать учетную запись</button>
