3b387e9070100f7d65006476ee6d33974f62eb26
Semen <IvanovSA2309@gmail.com>
2023-11-13 14:36:52 +0400

    classnames changes

--- client/src/component/Login/Login.tsx
+++ client/src/component/Login/Login.tsx
@@ -6,18 +6,18 @@ const Login: React.FC = () => {
 
 return (
     <div className='Login'>
-        <div className="logo"></div>
-        <div className="container">
-            <h3 className='containerHeader'>Войти</h3>
+        <div className="logoLogin"></div>
+        <div className="containerLogin">
+            <h3 className='containerLoginHeader'>Войти</h3>
             <form>
                 <input className='loginInput' placeholder='Логин'/><br />
                 <input className='loginInput' placeholder='Пароль'/><br />
                 <input type='checkbox' id='galka' className='checkbox'/><label className='checkboxText' htmlFor='galka'>Не выходить из учетной записи</label><br />
                 <button className='loginButton'>Продолжить</button><br />
                 <hr />
-                <div className='otherButtons'>
-                    <button className='otherButton'>Не можете войти?</button><br />
-                    <button className='otherButton'>Создать учетную запись</button>
+                <div className='otherButtonsLogin'>
+                    <button className='otherButtonLogin'>Не можете войти?</button><br />
+                    <button className='otherButtonLogin'>Создать учетную запись</button>
                 </div>
             </form>
         </div>
