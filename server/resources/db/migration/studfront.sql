2c3d598bf6ae6f0e21b731f11e605f73923d1dc6
StIvanovich <130762232+StIvanovich@users.noreply.github.com>
2023-11-26 21:31:44 +0400

    Сombining DB

--- server/resources/db/migration/studfront.sql
+++ server/resources/db/migration/studfront.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.5
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1:3306
--- Время создания: Ноя 14 2023 г., 10:41
--- Версия сервера: 10.3.13-MariaDB
--- Версия PHP: 7.1.22
+-- Время создания: Ноя 26 2023 г., 20:27
+-- Версия сервера: 10.4.12-MariaDB
+-- Версия PHP: 7.1.33
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -44,6 +43,24 @@ INSERT INTO `game` (`id`, `version`, `chat_hash`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Структура таблицы `gamers`
+--
+
+CREATE TABLE `gamers` (
+  `id` bigint(20) NOT NULL,
+  `name` varchar(120) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `user_id` bigint(20) DEFAULT NULL,
+  `person_id` bigint(20) DEFAULT NULL,
+  `experience` float DEFAULT NULL,
+  `hp` float DEFAULT NULL,
+  `money` float DEFAULT NULL,
+  `status` varchar(20) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `coordinate` point DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Структура таблицы `messages`
 --
 
@@ -63,6 +80,31 @@ INSERT INTO `messages` (`id`, `user_id`, `message`, `created`) VALUES
 (2, 1, 'hihihi', '2023-11-14 10:29:53'),
 (3, 4, 'hohohohohooooo', '2023-11-14 10:33:36'),
 (4, 4, 'иди в попу!', '2023-11-14 11:08:25');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `persons`
+--
+
+CREATE TABLE `persons` (
+  `id` bigint(20) NOT NULL,
+  `hp` float DEFAULT NULL,
+  `name` varchar(20) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `image` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `rangs`
+--
+
+CREATE TABLE `rangs` (
+  `id` bigint(20) NOT NULL,
+  `name` varchar(120) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `experience` float DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -98,9 +140,29 @@ ALTER TABLE `game`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Индексы таблицы `gamers`
+--
+ALTER TABLE `gamers`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `person_id` (`person_id`);
+
+--
 -- Индексы таблицы `messages`
 --
 ALTER TABLE `messages`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `persons`
+--
+ALTER TABLE `persons`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `rangs`
+--
+ALTER TABLE `rangs`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -121,16 +183,39 @@ ALTER TABLE `game`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
+-- AUTO_INCREMENT для таблицы `gamers`
+--
+ALTER TABLE `gamers`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT для таблицы `messages`
 --
 ALTER TABLE `messages`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
+-- AUTO_INCREMENT для таблицы `persons`
+--
+ALTER TABLE `persons`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT для таблицы `users`
 --
 ALTER TABLE `users`
   MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- Ограничения внешнего ключа сохраненных таблиц
+--
+
+--
+-- Ограничения внешнего ключа таблицы `gamers`
+--
+ALTER TABLE `gamers`
+  ADD CONSTRAINT `gamers_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `gamers_ibfk_2` FOREIGN KEY (`person_id`) REFERENCES `persons` (`id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
