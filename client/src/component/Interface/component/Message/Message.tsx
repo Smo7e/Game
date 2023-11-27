7f17230d7160868f213f8b31266ec36986005021
Smo7e <romayacovlev@gmail.com>
2023-11-27 22:50:22 +0400

    add Chat,Interface

---
+++ client/src/component/Interface/component/Message/Message.tsx
@@ -0,0 +1,15 @@
+import { useRef } from "react";
+import "./Message.css";
+interface MessageProps {
+    message: string;
+    name: string;
+}
+const Message: React.FC<MessageProps> = ({ message, name }) => {
+    return (
+        <div className="Message-Container">
+            <div className="message-name">{name}: </div>
+            <div className="message">&nbsp;{message} </div>
+        </div>
+    );
+};
+export default Message;
