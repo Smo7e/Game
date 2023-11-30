d51d2a89baae0d5952326611e022362665de786b
Константин <kostyagreduagin.ru@gmail.com>
2023-12-01 00:17:10 +0400

    Revert "Add files via upload"
    This reverts commit 97b8e77ddd59d7cc27af6f3f6f49679936794ca9.

--- client/src/modules/Game/Game.tsx
+++ client/src/modules/Game/Game.tsx
@@ -1,31 +1,8 @@
-import React, { useContext, useEffect } from "react";
+import React from "react";
 import { Canvas } from "@react-three/fiber";
-import { ServerContext, MediatorContext } from "../../App";
-import { TScene } from "../Server/types";
 import Player from "./Player";
 import Scene from "./Scene";
-
 const Game: React.FC = () => {
-    const server = useContext(ServerContext);
-    const mediator = useContext(MediatorContext);
-    const { GET_SCENE } = mediator.getEventTypes();
-
-    useEffect(() => {
-        server.startGameInterval();
-
-        const getSceneHandler = (scene: TScene) => {
-            console.log(scene);
-        }
-
-        mediator.subscribe(GET_SCENE, getSceneHandler);
-
-        return() => {
-            // отписка!!!
-            // mediator.unsubscribe(GET_SCENE, getSceneHandler);
-        }
-    });
-
-
     return (
         <Canvas
             camera={{
