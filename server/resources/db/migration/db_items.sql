a78e500f475e9c78dc40655b6a228ce4125d70b1
prostokoly <kolasosnov177@gmail.com>
2023-12-10 15:18:11 +0300

    fix_bug_41/2

--- server/resources/db/migration/db_items.sql
+++ server/resources/db/migration/db_items.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1:3306
--- Время создания: Дек 10 2023 г., 13:21
+-- Время создания: Дек 10 2023 г., 15:16
 -- Версия сервера: 10.3.22-MariaDB
 -- Версия PHP: 7.1.33
 
@@ -48,7 +48,7 @@ INSERT INTO `items` (`id`, `name`, `type`, `location`, `image`, `description`) V
 (5, 'Энергос', 'Расходник', 'Багетница', 'https://drive.google.com/file/d/1zOmnG7A1hi2swN4PRdhb5zP_phY0NOec/view?usp=drive_link', 'Восстанавливает здоровье на 15 единиц, на время боя увеличивает максимальное здоровье на 20 единиц'),
 (6, 'Учебник', 'Расходник', 'Программирование, русский язык, математика, английский язык', 'https://drive.google.com/file/d/1CcxSd3GCb92Krr4Of_iHkR82-iqm1PRV/view?usp=drive_link', 'Увеличивает урон по всем парам кроме “Физра” на 5 единиц'),
 (7, 'Сигареты', 'Расходник', 'Багетница', 'https://drive.google.com/file/d/1w8Rg20Ee_N4WXohJtQ6wclGg3mXmjPzX/view?usp=drive_link', 'Уменьшает текущее здоровье на 20, но на время боя увеличивает урон каждой атаки на 10 '),
-(8, 'Кофта “Stone Island” ', 'Одежда', 'Физра', 'https://drive.google.com/file/d/19xKs-LkbtI47TDBA22StocB-nvzkMaAv/view?usp=drive_link', ' Увеличивает максимальное здоровье на 15'),
+(8, 'Кофта “Stone Island” ', 'Одежда', 'Физра', 'https://drive.google.com/file/d/19xKs-LkbtI47TDBA22StocB-nvzkMaAv/view?usp=drive_link', 'Увеличивает максимальное здоровье на 15'),
 (9, 'Калькулятор', 'Гаджет', 'Математика', 'https://drive.google.com/file/d/14HEzCbJRFXuc9p0AjPwlTK2pp3j43dCo/view?usp=drive_link', 'Увеличивает урон по “Математике” на 15 единиц'),
 (10, 'Словарь', 'Книга', 'Русский язык, английский язык', 'https://drive.google.com/file/d/1Sms2iIUvf2aN3-EHdLz8-jMTyJLHhom0/view?usp=drive_link', 'Увеличивает урон по “Русскому языку” и “Английскому языку” на 10 единиц'),
 (11, 'Ноутбук', 'Гаджет', 'Математика, программирование', 'https://drive.google.com/file/d/1AmXmAyEcvSGUK0gL70VDyx_nLIhT7GQI/view?usp=drive_link', 'Увеличивает урон по “Программированию” и “Математике” на 10 единиц'),
